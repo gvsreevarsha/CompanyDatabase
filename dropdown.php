675327545bea23233bddb84e73987a23203bddba
Siddharth Datthathreya <marshtomp.1999@hotmail.com>
2020-11-12 20:35:41 +0530

    Dropdown for Organisation

--- dropdown.php
+++ dropdown.php
@@ -1,10 +1,10 @@
 <?php
 $con = new mysqli("localhost", "root", "NikiSiddu3014!", "Company_DB");
-$query = "SELECT distinct Origin FROM `cdb`";
+$query = "SELECT distinct Type_of_Organization FROM `cdb`";
 if ($result = $con->query($query)) {
     echo "<select name='Place'>";
     while ($row = $result->fetch_assoc()) {
-        echo "<option value='" . $row['Origin'] ."'>" . $row['Origin'] ."</option>";
+        echo "<option value='" . $row['Type_of_Organization'] ."'>" . $row['Type_of_Organization'] ."</option>";
     }
     $result->free();
 }
