219dddcb1e0ceb60ce59ff52d91172e9ab2f9823
varsha <gvsreevarsha@gmail.com>
2020-11-15 15:14:15 +0530

    97% done

--- index.php
+++ index.php
@@ -4,12 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    
     <title>Company Database</title>
 </head>
 <body class="bg-gradient-to-r from-teal-400 to-blue-500">
     <div>
-        <form class="h-50 border rounded my-10 mx-10" method="post" action="index.php">
+        <form class="h-50 border rounded my-10 mx-10" method="GET" action="index.php">
             <div class="flex flex-wrap justify-center my-10">
                 <input type="text" class="h-12 border rounded p-1 ml-4 sm:my-2 " placeholder="Company Name" name="c_name">
                 <select class="h-12 border rounded p-1 ml-4 sm:my-2 text-gray-500"  placeholder="Select an Industry" name="type">
@@ -45,51 +44,67 @@
     <div class="border rounded my-10 mx-10">
             <div id="Sort"></div>
             <hr class="bg-black"/>
+            <div id="pagination" class="p-2" align="right"></div>
             <div id="Companies">
                 <?php
-if(isset($_POST['city']) && isset($_POST['type']) && isset($_POST['c_name']))
-{
-$con = new mysqli("localhost", "root", "", "Companies");
-$city=$_POST['city'];
-$type=$_POST['type'];
-$name=$_POST['c_name'];
-$query = "SELECT * FROM `cdb` WHERE `Name_of_the_Company` LIKE '%$name%' AND `Type_of_Organization` LIKE '%$type%' AND `Company_Address` LIKE '%$city%' ORDER BY `Name_of_the_Company`";
-//echo $query;
-if ($result = $con->query($query)) {
-    while ($row = $result->fetch_assoc()) {
+                $row_cnt=0;   
+                $name='';
+                $type='';
+                $city='';         
+                if(isset($_GET['city']) && isset($_GET['type']) && isset($_GET['c_name']))
+                {
+                $con = new mysqli("localhost", "root", "", "Companies");
+                $city=$_GET['city'];
+                $type=$_GET['type'];
+                $name=$_GET['c_name'];
+                if(isset($_GET['page']))
+                    $page=$_GET['page'];
+                else
+                    $page=1;
+                $query = "SELECT * FROM `cdb` WHERE `Name_of_the_Company` LIKE '%$name%' AND `Type_of_Organization` LIKE '%$type%' AND `Company_Address` LIKE '%$city%' ORDER BY `Name_of_the_Company`";
+                if ($result = $con->query($query)) {
+                    $row_cnt=$result->num_rows;
+                }
+                $start=50*($page-1);
+                $query = "SELECT * FROM `cdb` WHERE `Name_of_the_Company` LIKE '%$name%' AND `Type_of_Organization` LIKE '%$type%' AND `Company_Address` LIKE '%$city%' ORDER BY `Name_of_the_Company` LIMIT ".$start.",50";
+                if ($result = $con->query($query)) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo '<div class="my-4 bg-gray-200 rounded ml-20 mr-20 p-2"><table width="100%">'.
+                        '<tr><td width="13%" style="vertical-align: text-top">Industry</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Name_of_the_Company"].'</td></tr>' .
+                        '<tr><td width="13%" style="vertical-align: text-top">Company Type</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Type_of_Organization"].'</td></tr> '.
+                        '<tr><td width="13%" style="vertical-align: text-top">Level of Office</td><td width="2%" style="vertical-align: text-top">:</td><td></td></tr> '.
+                        '<tr><td width="13%" style="vertical-align: text-top">Location</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Origin"].'</td></tr>'.
+                        '<tr><td width="13%" style="vertical-align: text-top">Phone No</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Contact_Person_Phone_No"].'</td></tr> '.
+                        '<tr><td width="13%" style="vertical-align: text-top">Website</td><td width="2%" style="vertical-align: text-top">:</td><td><a class="text-blue-500 hover:text-blue-800" href="'.$row["Website"].'">'.$row["Website"].'</a></td></tr>'.
+                        '</table></div>';  
+                        $row_cnt++;  
+                    }
+                    $result->free();
+                }
+        }
+        else
+        {
+        $con = new mysqli("localhost", "root", "", "Companies");
+        $query = "SELECT * FROM `cdb`";
+        $row_cnt=0;  
+        if ($result = $con->query($query)) {
+            while ($row = $result->fetch_assoc()) {
        
-        echo '<div class="my-4 bg-gray-200 rounded ml-20 mr-20 p-2"><table width="100%">'.
-        '<tr><td width="13%" style="vertical-align: text-top">Industry</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Name_of_the_Company"].'</td></tr>' .
-        '<tr><td width="13%" style="vertical-align: text-top">Company Type</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Type_of_Organization"].'</td></tr> '.
-        '<tr><td width="13%" style="vertical-align: text-top">Level of Office</td><td width="2%" style="vertical-align: text-top">:</td><td></td></tr> '.
-        '<tr><td width="13%" style="vertical-align: text-top">Location</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Origin"].'</td></tr>'.
-        '<tr><td width="13%" style="vertical-align: text-top">Phone No</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Contact_Person_Phone_No"].'</td></tr> '.
-        '<tr><td width="13%" style="vertical-align: text-top">Website</td><td width="2%" style="vertical-align: text-top">:</td><td><a class="text-blue-500 hover:text-blue-800" href="'.$row["Website"].'">'.$row["Website"].'</a></td></tr>'.
-        '</table></div>';    }
-    $result->free();
-}
-}
-else
-{
-$con = new mysqli("localhost", "root", "", "Companies");
-$query = "SELECT * FROM `cdb`";
-if ($result = $con->query($query)) {
-    while ($row = $result->fetch_assoc()) {
-       
-        echo '<div class="my-4 bg-gray-200 rounded ml-20 mr-20 p-2"><table width="100%">'.
-        '<tr><td width="13%" style="vertical-align: text-top">Industry</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Name_of_the_Company"].'</td></tr>' .
-        '<tr><td width="13%" style="vertical-align: text-top">Company Type</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Type_of_Organization"].'</td></tr> '.
-        '<tr><td width="13%" style="vertical-align: text-top">Level of Office</td><td width="2%" style="vertical-align: text-top">:</td><td></td></tr> '.
-        '<tr><td width="13%" style="vertical-align: text-top">Location</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Origin"].'</td></tr>'.
-        '<tr><td width="13%" style="vertical-align: text-top">Phone No</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Contact_Person_Phone_No"].'</td></tr> '.
-        '<tr><td width="13%" style="vertical-align: text-top">Website</td><td width="2%" style="vertical-align: text-top">:</td><td><a class="text-blue-500 hover:text-blue-800" href="'.$row["Website"].'">'.$row["Website"].'</a></td></tr>'.
-        '</table></div>';
+                echo '<div class="my-4 bg-gray-200 rounded ml-20 mr-20 p-2"><table width="100%">'.
+                '<tr><td width="13%" style="vertical-align: text-top">Industry</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Name_of_the_Company"].'</td></tr>' .
+                '<tr><td width="13%" style="vertical-align: text-top">Company Type</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Type_of_Organization"].'</td></tr> '.
+                '<tr><td width="13%" style="vertical-align: text-top">Level of Office</td><td width="2%" style="vertical-align: text-top">:</td><td></td></tr> '.
+                '<tr><td width="13%" style="vertical-align: text-top">Location</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Origin"].'</td></tr>'.
+                '<tr><td width="13%" style="vertical-align: text-top">Phone No</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Contact_Person_Phone_No"].'</td></tr> '.
+                '<tr><td width="13%" style="vertical-align: text-top">Website</td><td width="2%" style="vertical-align: text-top">:</td><td><a class="text-blue-500 hover:text-blue-800" href="'.$row["Website"].'">'.$row["Website"].'</a></td></tr>'.
+                '</table></div>';
+                $row_cnt++;  
+            }
+            $result->free();
+        }
     }
-    $result->free();
-}
-}
-?>
-            </div>
+    ?> 
+    </div>
     </div>
 </body>
 <script>
@@ -100,6 +115,20 @@ if ($result = $con->query($query)) {
     }
     document.getElementById("Sort").innerHTML=b;
 
+    var details="";
+</script>
+<script>
+    var i;
+    var b="";
+    var name="<?php echo $_GET['c_name'];?>";
+    var type="<?php echo $_GET['type'];?>";
+    var city="<?php echo $_GET['city'];?>";
+    var num="<?php echo $row_cnt;?>";
+    for(i=1;i<num/50+1;i++){
+        b+="<a href='index.php?c_name="+name+"&type="+type+"&city="+city+"&page="+i+"'><button class='text-white p-1 hover:bg-red border'>"+i+"</button></a>";
+    }
+    b+="&emsp;";
+    document.getElementById("pagination").innerHTML=b;
     var details="";
 </script>
 </html>
--- search.php
+++ search.php
@@ -12,7 +12,6 @@ $query1 = "SELECT * FROM cdb WHERE Name_of_the_Company LIKE '$let%'";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-
     <title>Company Database</title>
 </head>
 <body class="bg-gradient-to-r from-teal-400 to-blue-500">
@@ -55,28 +54,15 @@ $query1 = "SELECT * FROM cdb WHERE Name_of_the_Company LIKE '$let%'";
             <hr class="bg-black"/>
             <div id="Companies">
                 <?php
-$con = new mysqli("localhost", "root", "", "Companies");
-/*$city=$_POST['city'];
-$type=$_POST['type'];
-$name=$_POST['c_name'];
-$query2 = "SELECT * FROM `cdb` WHERE `Name_of_the_Company` LIKE '%$name%' AND `Type_of_Organization` LIKE '%$type%' AND `Company_Address` LIKE '%$city%' ORDER BY `Name_of_the_Company`";
-//echo $query;*/
-//$query= $query1." INTERSECT ".$query2;
-if ($result = $con->query($query1)) {
-    while ($row = $result->fetch_assoc()) {
+                    $con = new mysqli("localhost", "root", "", "Companies");
+                    if ($result = $con->query($query1)) {
+                        while ($row = $result->fetch_assoc()) {
        
-        echo '<div class="my-4 bg-gray-200 rounded ml-20 mr-20 p-2"><table width="100%">'.
-        '<tr><td width="13%" style="vertical-align: text-top">Industry</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Name_of_the_Company"].'</td></tr>' .
-        '<tr><td width="13%" style="vertical-align: text-top">Company Type</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Type_of_Organization"].'</td></tr> '.
-        '<tr><td width="13%" style="vertical-align: text-top">Level of Office</td><td width="2%" style="vertical-align: text-top">:</td><td></td></tr> '.
-        '<tr><td width="13%" style="vertical-align: text-top">Location</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Origin"].'</td></tr>'.
-        '<tr><td width="13%" style="vertical-align: text-top">Phone No</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Contact_Person_Phone_No"].'</td></tr> '.
-        '<tr><td width="13%" style="vertical-align: text-top">Website</td><td width="2%" style="vertical-align: text-top">:</td><td><a class="text-blue-500 hover:text-blue-800" href="'.$row["Website"].'">'.$row["Website"].'</a></td></tr>'.
-        '</table></div>';
-    }
-    $result->free();
-}
-?>
+                            echo '<div class="my-4 bg-gray-200 rounded ml-20 mr-20 p-2"><table width="100%">'.'<tr><td width="13%" style="vertical-align: text-top">Industry</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Name_of_the_Company"].'</td></tr>' .'<tr><td width="13%" style="vertical-align: text-top">Company Type</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Type_of_Organization"].'</td></tr> '.'<tr><td width="13%" style="vertical-align: text-top">Level of Office</td><td width="2%" style="vertical-align: text-top">:</td><td></td></tr> '.'<tr><td width="13%" style="vertical-align: text-top">Location</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Origin"].'</td></tr>'.'<tr><td width="13%" style="vertical-align: text-top">Phone No</td><td width="2%" style="vertical-align: text-top">:</td><td>'.$row["Contact_Person_Phone_No"].'</td></tr> '.'<tr><td width="13%" style="vertical-align: text-top">Website</td><td width="2%" style="vertical-align: text-top">:</td><td><a class="text-blue-500 hover:text-blue-800" href="'.$row["Website"].'">'.$row["Website"].'</a></td></tr>'.'</table></div>';
+                        }
+                        $result->free();
+                    }
+                    ?>
             </div>
     </div>
 </body>
